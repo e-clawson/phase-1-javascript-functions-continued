f4bb5cfb17bb8fc16887c0cd5953de6d48554e4a
Elizabeth Clawson <emclawson1@gmail.com>
2021-07-26 00:22:17 -0500

    commit tree b2e4c5c1a57715e474b4b14316762a6ac28ce8bd

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function f(place = 'go to the office') {
+    return `This Monday, I will ${place}.`;
+}
+mondayWork('work from home');
+
+// function outer(greeting, msg="It's a fine day to learn") { // 2
+//     const innerFunction =  function(name, lang="Python") { // 3
+//       return `${greeting}, ${name}! ${msg} ${lang}`; // 4
+//     }
+//     return innerFunction("student", "JavaScript"); // 5
+//   }
+  
+//   outer("Hello"); // 1
+//   //=> "Hello, student! It's a fine day to learn JavaScript" 
+
+function wrapAdjective(symbol = '*') {
+    return function(compliment = "a hard worker" ) {
+        return `You are ${symbol}${compliment}${symbol}!`
+    }
+   }
+wrapAdjective('||','a dedicated programmer');
