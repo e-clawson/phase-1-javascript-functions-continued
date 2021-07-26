4c12aa8e34c554b0e9bafcc19b5a6dc1fb6eaa3f
Elizabeth Clawson <emclawson1@gmail.com>
2021-07-26 00:23:17 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function f(place = 'go to the office') {
+    return `This Monday, I will ${place}.`;
+}
+mondayWork('work from home');
+
+function wrapAdjective(symbol = '*') {
+    return function(compliment = "a hard worker" ) {
+        return `You are ${symbol}${compliment}${symbol}!`
+    }
+   }
+wrapAdjective('||','a dedicated programmer');
